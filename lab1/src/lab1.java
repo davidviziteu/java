73d466ef83bdf699ea4300a419c41778ba3b958e
David Viziteu <andrei_david7@hotmail.com>
2021-02-15 09:08:58 +0200

    compulsory part done

---
+++ lab1/src/lab1.java
@@ -0,0 +1,23 @@
+public class lab1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        int n = (int) (Math.random() * 1000000);
+        n *= 3;
+        String binaryString="10101";
+        n += Integer.parseInt(binaryString, 2);
+        String hexaString="FF";
+        n += Integer.parseInt(hexaString, 16);
+        n *= 6;
+        int result = n;
+        while(result != result % 10){
+            n = result;
+            result = 0;
+            while(n != 0){
+                result += n % 10;
+                n /= 10;
+            }
+        }
+        System.out.println("Willy-nilly, this semester I will learn " + languages[result]);
+    }
+}
