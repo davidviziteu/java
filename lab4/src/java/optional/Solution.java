8ea24142973e3036a008d0fc5611e4d0e92793dc
David Viziteu <andrei_david7@hotmail.com>
2021-03-14 17:35:39 +0200

    added faker

--- lab4/src/java/optional/Solution.java
+++ lab4/src/java/optional/Solution.java
@@ -1,4 +1,17 @@
 package optional;
 
+import compulsory.School;
+import compulsory.Student;
+
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
+    Map<School, List<Student>> schoolPrefs;
+    Map<Student, List<School>> studPrefs;
+    public Solution(Map<Student, List<School>> studPrefs, Map<School, List<Student>> schoolPrefs){
+        this.schoolPrefs = schoolPrefs;
+        this.studPrefs = studPrefs;
+    }
+
 }
