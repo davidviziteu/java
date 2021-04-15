1cb0c5a61a18db93f552fc87eaaf5459e09e19cf
David Viziteu <andrei_david7@hotmail.com>
2021-04-15 09:18:02 +0300

    compulsory semi-done

--- lab8/src/main/java/compulsory/DataBase.java
+++ lab8/src/main/java/compulsory/DataBase.java
@@ -1,13 +1,37 @@
 package compulsory;
 
 
-public class DataBaseSingleton {
-    static Connection db = null;
+import java.sql.*;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
 
-    static public DataBaseSingleton getInstance(){
-        if(db == null)
-            db = new Connection();
-        return db;
+public class DataBase {
+    Connection db = null;
+    static DataBase instance = null;
+
+    static public DataBase getInstance(){
+        if(instance == null)
+            instance = new DataBase();
+        return instance;
     }
 
+    DataBase(){
+        try{
+            db = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "STUDENT", "STUDENT");
+        }catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void test(String name){
+        try {
+            Statement stmt = db.createStatement( );
+            String sql = "SELECT * FROM PROFESORI";
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println(rs);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
--- lab8/src/main/java/compulsory/Main.java
+++ lab8/src/main/java/compulsory/Main.java
@@ -1,2 +1,15 @@
-package compulsory;public class Main {
+package compulsory;
+
+
+import com.sun.jdi.connect.spi.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    static public void main(String[] args) {
+        var db = DataBase.getInstance();
+        db.test("aa");
+    }
+
 }
